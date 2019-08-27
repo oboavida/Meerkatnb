34b8620b8b5d325019886cc4467ae633e09e1407
Carolina Vieira <carolina.vieira@novasbe.pt>
2019-08-27 12:37:26 +0100

    faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,3 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(name: 'Rita', location: 'Lisbon', description: 'BLBLBLBLL', email:'anfff00@gmail.com', password:'123456')
-
-Meerkat.create!(user_id: 1, name: 'Antonio', specialty: 'cooker', age: 5, price: 5, location: 'lisbon', description: 'Blablablabalalabalaab')
