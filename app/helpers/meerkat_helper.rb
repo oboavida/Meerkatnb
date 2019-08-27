8d9cebfbf0cc7f9f810cb7a3633cdce9c2ce4334
Antonio Boavida <antonioruela@gmail.com>
2019-08-27 12:20:24 +0200

    Fixed bug on index

--- app/helpers/meerkat_helper.rb
+++ app/helpers/meerkat_helper.rb
@@ -2,7 +2,7 @@ module MeerkatHelper
   def photo_or_fallback(meerkat)
     if meerkat.photo.present?
       # cl_image_tag # => generates a <img> tag
-      cl_image_path(cocktail.photo)
+      cl_image_path(meerkat.photo)
     else
       asset_path('meerkat_rezdy_lg.jpg')
       # image_tag # => generates a <img> tag
