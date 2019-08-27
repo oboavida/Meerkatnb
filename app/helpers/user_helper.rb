a05727c0c1acabb08560197e1ba43b7dc82e384f
Antonio Boavida <antonioruela@gmail.com>
2019-08-27 17:23:26 +0200

    setting default profile pic

---
+++ app/helpers/user_helper.rb
@@ -0,0 +1,11 @@
+module UserHelper
+  def photo_or_fallback(user)
+    if user.photo.present?
+      # cl_image_tag # => generates a <img> tag
+      cl_image_path(user.photo)
+    else
+      asset_path('default_profile_pic.gif')
+      # image_tag # => generates a <img> tag
+    end
+  end
+end
