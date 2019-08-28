a1ff52313bc25fd486fafcb3a8f4cddbc656b5e6
Carolina Vieira <carolina.vieira@novasbe.pt>
2019-08-28 15:16:00 +0100

    more shit

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.destroy_all
-Meerkat.destroy_all
+# User.destroy_all
+# Meerkat.destroy_all
 
 User.create!(
     name: Faker::Name.unique.name,
